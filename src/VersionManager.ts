22ac72a09d86fc6eea3dbed3284bdbae9c69fa87
Krigston <sedovwwwwb.bbk.r1235@gmail.com>
2025-08-29 11:26:41 +0700

    🔧 Откорректирована версия и улучшен Git hook
    - Версия возвращена к правильной 1.2.1 (исправления багов)
    - Улучшена логика Git hook для определения типа версии
    - Исправлена проблема с получением commit message
    - Теперь 🐛 emoji корректно определяется как PATCH

--- src/VersionManager.ts
+++ src/VersionManager.ts
@@ -1,6 +1,6 @@
 export class VersionManager {
     // Версия из package.json (будет обновляться автоматически)
-    public static readonly CURRENT_VERSION = '2.0.0';
+    public static readonly CURRENT_VERSION = '1.2.2';
     
     public static getVersion(): string {
         return this.CURRENT_VERSION;
